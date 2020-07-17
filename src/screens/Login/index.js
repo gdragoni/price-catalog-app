e66b739b220fc731dd16a00aae3f742b43f10b14
Gabriel Dragoni <ga.dragoni@gmail.com>
2020-07-16 23:25:34 -0300

    login screen logo

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,27 +1,49 @@
 import * as React from 'react';
-import { StyleSheet, SafeAreaView, View, Text, Image } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, ImageBackground, Image, Dimensions } from 'react-native';
 
 export default function LoginScreen() {
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={require('../../../assets/images/login/login-background.jpg')} style={styles.backgroundImage} />
+            <ImageBackground source={require('../../../assets/images/login/login-background.jpg')} style={styles.imageBackground}>
+                <View style={styles.topSpaceView}>
+                    <Image source={require('../../../assets/images/logo/offer-logo-purple.png')} style={styles.logo} />
+                </View>
+                <View style={styles.bottomSpaceView}>
 
-            <Text style={{
-                fontFamily: 'OpenSans-Bold'
-            }}>Login Screen</Text>
+                </View>
+            </ImageBackground>
         </SafeAreaView>
     );
 }
+
+const halfWidthScreen = Dimensions.get('window').width/2
+const logoProportion = 616/792 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
         alignItems: 'center', 
         justifyContent: 'center',
-        
     },
-    backgroundImage: {
+    imageBackground: {
       flex: 1,
       resizeMode: 'cover',
+      width: '100%',
+      height: '100%',
+    },
+    topSpaceView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: halfWidthScreen,
+        height: halfWidthScreen*logoProportion,
+        resizeMode: 'cover',
+        borderRadius: 12,
+        overflow: 'hidden',
+    },
+    bottomSpaceView: {
+        flex: 1,
     },
 });
